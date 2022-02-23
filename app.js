c17f79e21e2d3189510640c6598bed48e380de88
Mo45 <ivioninjs@gmail.com>
2022-02-23 21:14:35 +0600

    1. Added nodemon.json for ignore json files 2. Turned into ESmodule 3. written function for get words by length of letters

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const raw = require("./draft.json");
+import { readFile } from "fs/promises";
+
+const raw = JSON.parse(
+  await readFile(new URL("./draft.json", import.meta.url))
+);
 
 const formatted = raw.map(element => {
   let withoutBreakLines = element.description.split("\n");
@@ -9,3 +13,12 @@ const formatted = raw.map(element => {
   let removeDuplicate = withoutEmptySpace.slice(1);
   return { word: element.word, definition: removeDuplicate };
 });
+
+function getWordsByLength(words, number) {
+  return words.filter(element =>
+    element.word.length == number ? element : false
+  );
+}
+
+const dictionary = getWordsByLength(formatted, 5);
+console.log(dictionary);
